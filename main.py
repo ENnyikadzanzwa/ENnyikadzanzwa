3b785b0b9e91916374287dee25adf6b4b540b3c3
ENnyikadzanzwa <164345772+ENnyikadzanzwa@users.noreply.github.com>
2024-03-29 15:16:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,8 @@ with col2:
     st.header("Other Skills")
     skills = ["Fundamentals of cybersecurity", """Artificial Intelligence \n
     Large language models,Fine tuning""", """Web Design\n
-              HTML,CSS AND JAVASCRIPT""","APIs"]
+              HTML,CSS AND JAVASCRIPT""","APIs","""Operating Systems\n
+              linux and windows""","Microsoft Office Suite"]
     for skill in skills:
         st.info(skill)
 
